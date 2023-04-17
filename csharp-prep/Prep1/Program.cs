56db7b55b8dc0d007d33578a36a2db7ace276be7
Lmhprap <121910511+Lmhprap@users.noreply.github.com>
2023-04-17 13:25:57 -0600

    Finish Prep1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,12 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
+        // Ask the user for their first name.
         Console.Write("What is your first name? ");
         string first = Console.ReadLine();
 
+        //Ask the user for their last name.
         Console.Write("What is your last name? ");
         string last = Console.ReadLine();
 
+        //List the name.
         Console.WriteLine($"Your name is {last}, {first} {last}");
     }
 }
