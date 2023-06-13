cac522eb137b3d3ba5502b3368cbfe55b2320bb3
Lmhprap <121910511+Lmhprap@users.noreply.github.com>
2023-06-13 10:46:08 -0600

    Color Change

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -9,13 +9,13 @@ class Program
         // the class inherits from Shape
         List<Shape> shapes = new List<Shape>();
 
-        Square s1 = new Square("Red", 3);
+        Square s1 = new Square("Blue", 3);
         shapes.Add(s1);
 
-        Rectangle s2 = new Rectangle("Blue", 4, 5);
+        Rectangle s2 = new Rectangle("Green", 4, 5);
         shapes.Add(s2);
 
-        Circle s3 = new Circle("Green", 6);
+        Circle s3 = new Circle("Purple", 6);
         shapes.Add(s3);
 
         foreach (Shape s in shapes)
